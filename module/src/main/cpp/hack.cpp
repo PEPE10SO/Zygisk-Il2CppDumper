2cd39b6d67e5bc135e023c7f068750f4e9077cb6
HuoyerSohel <pepezzm10s@gmail.com>
2025-05-23 11:15:37 -0500

    Actualizar hack.cpp

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -1,3 +1,26 @@
+// Copyright (c) 2020-2021 Perfare
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+// Created by Perfare on 2020/7/4.
+
 #include "hack.h"
 #include "il2cpp_dump.h"
 #include "log.h"
@@ -398,3 +421,18 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
     return JNI_VERSION_1_6;
 }
 #endif
+
+----- ARCHIVO: hack.h -----
+
+//
+// Created by Perfare on 2020/7/4.
+//
+
+#ifndef ZYGISK_IL2CPPDUMPER_HACK_H
+#define ZYGISK_IL2CPPDUMPER_HACK_H
+
+#include <stddef.h>
+
+void hack_prepare(const char *game_data_dir, void *data, size_t length);
+
+#endif //ZYGISK_IL2CPPDUMPER_HACK_H
